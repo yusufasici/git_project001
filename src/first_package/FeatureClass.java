0c15aeca4df90ba4aba34d1aa9114fd3e67ca106
yusufasici <68306103+yusufasici@users.noreply.github.com>
2020-08-08 15:57:53 -0400

    Update FeatureClass.java

--- src/first_package/FeatureClass.java
+++ src/first_package/FeatureClass.java
@@ -5,6 +5,7 @@ public class FeatureClass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is my commit from feature branch");
+		System.out.println("First Commit from gitHub(Remote) Master branch.");
 		
 	}
 
