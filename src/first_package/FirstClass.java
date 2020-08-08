fd195cf83b5657eaca2f5acf03a4512577d64f10
Y. Ziya Asici <yusuf.asici@gmail.com>
2020-08-08 12:30:49 -0400

    Third local commit

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,8 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Our first commit from local repo");
-		System.out.println("Our second commit from local repo");
-		
+		System.out.println("Our second commit local repo CHANGES HERE");
+		System.out.println("Our third commit from local repo");
 	}
 
 }
