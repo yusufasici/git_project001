876bab3a570a1889d3c532262c3eb07c31180a7b
Y. Ziya Asici <yusuf.asici@gmail.com>
2020-08-08 12:29:08 -0400

    Second local commit

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Our first commit from local repo");
+		System.out.println("Our second commit from local repo");
 		
 	}
 
